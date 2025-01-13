d65bd10e7730efa469beb104045edf3bd5bdcdb5
Bineeta123 <khanalbineeta3@gmail.com>
2025-01-13 08:29:22 +0545

    message

--- sathi/src/app/profile/page.tsx
+++ sathi/src/app/profile/page.tsx
@@ -2,8 +2,8 @@ import EmployeeInfo from "@/components/EmployeeInfo";
 
 export default function profile(){
     return (
-        <div style={{display:'flex', justifyContent:'center',}}>
-            <EmployeeInfo image='/Girl1.jpeg' name='Sahara Ghimire' age='20' gender='female' address='Balkumari,Lalitpur' nationality='Nepali' qualification='BE Software'  />
+        <div style={{display:'flex', justifyContent:'center'}}>
+            <EmployeeInfo image='/Girl1.jpeg' name='Rita BK' age='20' gender='female' address='Balkumari,Lalitpur' nationality='Nepali' qualification='BE Software'  />
         </div>
     )
 }
